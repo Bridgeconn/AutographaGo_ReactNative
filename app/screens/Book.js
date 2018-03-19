12f586206c51e6524ace3f94489a9f01d7a2145c
neetuy <neetuy1455@gmail.com>
2018-03-19 17:31:39 +0530

    converted simple grid to flatList in Book page chapter number

--- app/screens/Book.js
+++ app/screens/Book.js
@@ -4,42 +4,37 @@ import {
   Text,
   View,
   Button,
-  Dimensions
+  Dimensions,
+  FlatList
 } from 'react-native';
 
-const height = Dimensions.get('window').height-80;
 const width = Dimensions.get('window').width;
 
 export default class Book extends Component {
+
   static navigationOptions = {
     headerTitle: 'Book',
   };
-  constructor(){
-  super()
-  this.state = {
-      numberofCol:[1,2,3,4,5],
-      numberOfRow:[1,2,3,4,5,6,7],
-      number:[],
-  }
-}
 
+  constructor(){
+    super()
+    this.state = {
+        numberOfRow:[1,2,3,4,5,6,7,8,9]
+    }
+  }
 
   render() {
     return (
       <View style={{flex:1}}>
-      <View style={{flex:1,flexDirection:'column',justifyContent:"center",alignItems:"center",top:0}}>
-      {
-        this.state.numberOfRow.map((row,index1)=>
-          <View style={{flexDirection:'row',height:height/7}}>
-          {
-            this.state.numberofCol.map((col,index2)=>
-              <View style={{flex:1,flexDirection:'column',borderColor:'#282d35',borderWidth:1,justifyContent:"center"}}>
-                <Text style={{textAlign:"center",alignItems:"center"}}></Text>
-              </View>
-         )}
-         </View>
-      )}
-      </View>
+      <FlatList
+        numColumns={4}
+        data={this.state.numberOfRow}
+        renderItem={({item}) => 
+        <View style={{flex:0.25,borderColor:'black',borderRightWidth:1, borderBottomWidth:1,
+          height:width/4, justifyContent:"center"}}>
+        <Text style={{textAlign:"center",alignItems:"center"}}>{item}</Text>
+      </View>}
+      />
       </View>  
     );
   }
