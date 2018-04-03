0c1613324673200071d2209b504387031c4c967c
Prerna Singh <prerna11082@iiitd.ac.in>
2018-04-03 17:27:21 +0530

    adding styling to verse text, still needs fix

--- app/components/VerseViewBook.js
+++ app/components/VerseViewBook.js
@@ -29,53 +29,88 @@ export default class VerseViewBook extends Component {
       case Constants.MarkerTypes.VERSE: {
         if (this.state.verseData.verseNumber == "1" || 
             this.state.verseData.verseNumber.startsWith("1-")) {
+              var resString = "";
+              var initString = this.state.verseData.text;
+              var temp = initString.split(' ');
+              for (var i=0; i<temp.length; i++) {
+                switch (temp[i]) {
+                  case Constants.MarkerConstants.MARKER_NEW_PARAGRAPH: {
+                    resString = resString + "\n";
+                    break;
+                  }
+                  case Constants.StylingConstants.MARKER_Q: {
+                    resString = resString + "\n    ";
+                    break;
+                  }
+                  default: {
+                    resString = "";
+                    break;
+                  }
+                }
+              }
               return (
-                <TextWithLetterSpacing spacing={this.letterSpacing} children={this.state.verseData.text} />
+                <Text style={{fontSize:16, backgroundColor: 'yellow'}} 
+                  letterSpacing={this.letterSpacing} >
+                  {this.state.verseData.text}
+                </Text>
               );
         }
         return (
           <Text>
-            <TextWithLetterSpacing 
-              textStyle={{fontSize:10}}
-              spacing={this.letterSpacing} 
-              children={this.state.verseData.verseNumber} />
-            <TextWithLetterSpacing 
-              textStyle={{textDecorationLine: this.state.isSelected ? 'underline' : 'none'}}
-              spacing={this.letterSpacing} 
-              children={ this.state.verseData.text} />            
+            <Text style={{fontSize:10}} letterSpacing={this.letterSpacing} > 
+              {this.state.verseData.verseNumber}
+            </Text>
+            <Text style={{fontSize:16, textDecorationLine: this.state.isSelected ? 'underline' : 'none'}} 
+              letterSpacing={this.letterSpacing} >
+              { this.state.verseData.text}
+            </Text>         
           </Text>
         );
       }
       case Constants.MarkerTypes.PARAGRAPH: {
         if (this.state.verseData.verseNumber == "1" || 
             this.state.verseData.verseNumber.startsWith("1-")) {
-              return null;
+              return (
+                <Text style={{fontSize:16}} letterSpacing={this.letterSpacing} >
+                  {this.state.verseData.text}
+                </Text>      
+              );
         }
         return (
-          <TextWithLetterSpacing 
-            spacing={this.letterSpacing} 
-            children={"\n"} />
+          <Text style={{fontSize:16}} letterSpacing={this.letterSpacing} >
+            {"\n"} {this.state.verseData.text}
+          </Text>
         );
       }
       case Constants.MarkerTypes.SECTION_HEADING: {
-        return (
-          <TextWithLetterSpacing 
-            textStyle={{fontSize:20}} 
-            spacing={this.letterSpacing} 
-            children={this.state.verseData.text} />
-        );
       }
       case Constants.MarkerTypes.SECTION_HEADING_ONE: {
-        return null;        
+        return (
+          <Text style={{fontSize:24}} letterSpacing={this.letterSpacing} >
+            {this.state.verseData.text}
+          </Text>
+        );        
       }
       case Constants.MarkerTypes.SECTION_HEADING_TWO: {
-        return null;
+        return (
+          <Text style={{fontSize:22}} letterSpacing={this.letterSpacing} >
+            {this.state.verseData.text}
+          </Text>
+        );
       }
       case Constants.MarkerTypes.SECTION_HEADING_THREE: {
-        return null;
+        return (
+          <Text style={{fontSize:20}} letterSpacing={this.letterSpacing} >
+            {this.state.verseData.text}
+          </Text>
+        );
       }
       case Constants.MarkerTypes.SECTION_HEADING_FOUR: {
-        return null;        
+        return (
+          <Text style={{fontSize:18}} letterSpacing={this.letterSpacing} >
+            {this.state.verseData.text}
+          </Text>
+        );      
       }
       default: {
         return null;
