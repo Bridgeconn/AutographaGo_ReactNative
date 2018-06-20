c18ba6679a59b37e87bb4b8996f6d0e964dcca5b
neetuy <neetuy1455@gmail.com>
2018-06-20 17:39:14 +0530

    working on showing loader until data load from db

---
+++ app/components/SearchTab.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import {Button,Segment,Text, Container, Content,Header,Left,Right,Body,Title} from 'native-base'
+
+
+const SearchResultTypes = {
+    ALL: 0,
+    OT: 1,
+    NT: 2
+  };
+
+export default class SearchTab extends Component {
+   
+    constructor(props){
+        console.log("props value in searchtab"+props.activeTab)
+        super(props);
+
+    }
+    render(){
+    return (
+            <Segment style={{height:32,marginHorizontal:6,marginVertical:16}}>
+                <Button 
+                style={{paddingRight:0,paddingLeft:0}}
+                onPress={() =>this.props.toggleFunction(SearchResultTypes.ALL)} 
+                active={this.props.activeTab == SearchResultTypes.ALL ? true : false} 
+                >
+                <Text>
+                    All
+                </Text>
+                </Button>
+                <Button 
+                style={{paddingRight:0,paddingLeft:0}}
+                onPress={() =>this.props.toggleFunction(SearchResultTypes.OT)}  
+                active={this.props.activeTab == SearchResultTypes.OT ? true : false} 
+                >
+                <Text >
+                    Old Testament
+                </Text>
+                </Button>
+                <Button 
+                style={{paddingRight:0,paddingLeft:0}}
+                onPress={()=>this.props.toggleFunction(SearchResultTypes.NT)} 
+                active={this.props.activeTab == SearchResultTypes.NT ? true : false} 
+                >
+                <Text >
+                    New Testament
+                </Text>
+                </Button>
+            </Segment> 
+     )
+    }
+}
--- app/screens/Search/Search.js
+++ app/screens/Search/Search.js
@@ -3,72 +3,261 @@ import {
   StyleSheet,
   Text,
   View,
-  Button,
+  // Button,
   TouchableOpacity,
   TextInput,
-  FlatList
+  FlatList,
+  ActivityIndicator,
+  VirtualizedList,
+  Dimensions
 } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons'
 import DbQueries from '../../utils/dbQueries.js'
-import { Item } from 'native-base';
+import {getBookNameFromMapping, getBookNumberFromMapping} from '../utils/UtilFunctions'
+const width = Dimensions.get('window').width-20;
+import SearchTab from '../../components/SearchTab'
+import { Segment } from 'native-base';
+
+const SearchResultTypes = {
+  ALL: 0,
+  OT: 1,
+  NT: 2
+};
 
 export default class Search extends Component {
+  
+  static navigationOptions = ({navigation}) =>({
+    headerTitle: (<TextInput
+        placeholder="Search"
+        ref={ref => clear = ref}
+        style={{width:width}}
+        onChangeText={(text) => navigation.state.params.onTextChange(text)}
+        returnKeyType="search"
+        onSubmitEditing={() => navigation.state.params.onSearchText()}
+    />),
+   
+    headerRight:(
+        <Icon name="search" size={36} onPress={()=>navigation.state.params.onSearchText()}/>
+      )
+  })
+
 
   constructor(){
     super();
     this.state = {
-      searchedResult:[]
+      searchedResult:[],
+      activeTab:SearchResultTypes.ALL,
+      isLoading:false,
+      tabsData:[]
     }
-    // this.onSearchText = this.onSearchText.bind(this)
+    this.onSearchText = this.onSearchText.bind(this)
+    this.clearData = this.clearData.bind(this)
+    this.toggleButton = this.toggleButton.bind(this)
   }
 
-  static navigationOptions = ({navigation}) =>({
-    headerTitle: (<TextInput
-        placeholder="Search"
-        style={{width:50, textAlignVertical: "top"}}
-        onChangeText={(text) => navigation.state.params.onText(text)}
-    />),
-    // headerRight:(
-    //     <Icon name="search" size={36} onPress={()=>navigation.state.params.onSearchText()}/>
-    //   )
-  })
+  
   async onSearchText(){
-    console.log("waiting for search funtion ")
-    // let searchResultByBookName = await DbQueries.querySearchBookWithName(this.state.text,"ULB", "ENG");
-    let searchResultByVerseText = await DbQueries.querySearchVerse(this.state.text,"ULB", "ENG")
-     if (searchResultByVerseText && searchResultByVerseText.length > 0 ) {
-       console.log("search result "+JSON.stringify(searchResultByVerseText[0].text))
-      this.setState({searchedResult: searchResultByVerseText}) 
-     }   
-  }
-  onText = (text) =>{
-    this.setState({text})
+       this.setState({isLoading:true,searchedResult:[], tabsData:[] })
+        let searchResultByBookName = await DbQueries.querySearchBookWithName("ULB", "ENG",this.state.text);
+        if(searchResultByBookName && searchResultByBookName.length >0 ){
+          for(var i = 0; i < searchResultByBookName.length ;i++ ){
+            let reference = { bookId:searchResultByBookName[i].bookId,
+              bookName:getBookNameFromMapping(searchResultByBookName[i].bookId),
+              bookNumber: getBookNumberFromMapping(searchResultByBookName[i].bookId),
+              chapterNumber:1,
+              verseNumber:"1",
+              versionCode:'ULB',
+              languageCode:'ENG',
+              type: 'v',
+              text: 'ASDXFCGVBHNM',
+              highlighted: 'false' }
+              this.setState(
+                {searchedResult:[...this.state.searchedResult, reference]
+              }
+            )
+    
+              this.addReferenceToTab(reference)
+            
+          }
+         }
+         let searchResultByVerseText = await DbQueries.querySearchVerse("ULB","ENG",this.state.text)
+         if (searchResultByVerseText &&  searchResultByVerseText.length >0) {
+           for(var i = 0; i < searchResultByVerseText.length ;i++ ){
+             let reference = {bookId:searchResultByVerseText[i].bookId,
+               bookName:getBookNameFromMapping(searchResultByVerseText[i].bookId),
+               bookNumber:getBookNumberFromMapping(searchResultByVerseText[i].bookId),
+               chapterNumber:searchResultByVerseText[i].chapterNumber,
+               verseNumber:searchResultByVerseText[i].verseNumber,
+               versionCode:searchResultByVerseText[i].versionCode,
+               languageCode:searchResultByVerseText[i].languageCode,
+               type: searchResultByVerseText[i].type,
+               text: searchResultByVerseText[i].text,
+               highlighted: searchResultByVerseText[i].highlighted}
+     
+               this.setState(
+                 // prevState =>({
+                 {searchedResult:[...this.state.searchedResult, reference]
+               })
+             // )
+     
+               this.addReferenceToTab(reference)
+           }
+          }
+   
+     // todo fix loading true when all references added
+     this.setState({isLoading:false})
 
-    console.log("text"+this.state.text)
+  }
+  clearData(){
+    console.log("params value "+JSON.stringify(navigation.state))
+
+  }
+  addReferenceToTab(reference) {
+    console.log("reference " +reference)
+    switch (this.state.activeTab) {
+      case SearchResultTypes.ALL: {
+        this.setState(
+          {tabsData:[...this.state.tabsData, reference]
+        })
+        break
+      }
+      case SearchResultTypes.OT: {
+        if(reference.bookNumber < 40){
+          this.setState(
+            {tabsData:[...this.state.tabsData, reference]
+          })
+        }                
+        break
+      }
+      case SearchResultTypes.NT: {
+        if(reference.bookNumber > 40){
+          this.setState(
+            {tabsData:[...this.state.tabsData, reference]
+          })
+        }     
+        break
+      }
+    }
+  }
+
+  renderDataOnPressTab(activeTab){
+    this.setState({tabsData: []}, () => {
+      switch (activeTab) {
+        case SearchResultTypes.ALL: {
+            this.setState({tabsData: this.state.searchedResult})
+          break
+        }
+        case SearchResultTypes.OT: {
+          let data = [];
+          for(var i = 0; i < this.state.searchedResult.length ;i++ ){
+            if(this.state.searchedResult[i].bookNumber < 40){
+              data.push(this.state.searchedResult[i])
+            }
+          }
+          this.setState({tabsData:data})
+          break
+        }
+        case SearchResultTypes.NT: {
+          let data = [];
+          for(var i = 0; i < this.state.searchedResult.length ;i++ ){
+            if(this.state.searchedResult[i].bookNumber > 40){
+              data.push(this.state.searchedResult[i])
+            }
+          }
+          this.setState({tabsData:data})
+          break
+        }
+      }
+    })
+  }
+
+  onTextChange = (text) =>{
+    this.setState({text})
   }
   
   componentDidMount(){
-    // console.log("props from navigation options "+this.props.navigation.state.params.text)
-    this.props.navigation.setParams({onText: this.onText,onSearchText: this.onSearchText,text:this.state.text})
+    this.props.navigation.setParams({onTextChange: this.onTextChange,
+      onSearchText: this.onSearchText,
+      text:this.state.text,
+      onChangeText:this.onChangeText,
+      clearData:this.clearData
+    })
   }
 
-  render() {
+  toggleButton(activeTab){
+    console.log("toggle function "+activeTab)
+      if (this.state.activeTab == activeTab) {
+        console.log(" this.state.activeTab "+this.state.activeTab+ " active tab " +activeTab)
+        return
+      }
+      this.setState({activeTab}, ()=> {
+          this.renderDataOnPressTab(activeTab)
+      })
+  }
+
+  ListEmptyComponent = () =>{
     return (
-      <View>
-      <Icon name="search" size={36} onPress={()=>this.onSearchText()}/>
-      <FlatList
-          data={this.state.searchedResult}
-          renderItem={({item}) => 
-          <View>
-            <View>
-              <Text style={{color:"red"}}> {item.bookId} {item.verseNumber} : {item.chapterNumber} </Text>
-            </View>
-          <Text>{item.text}</Text>
-          
-         
-          </View>
-          }/>
+      <View style={{alignSelf:'center'}}>
+      {this.state.isLoading == false && this.state.tabsData == null ? 
+      <Text>No Result Found</Text>:null
+      } 
       </View>
+    )
+  }
+  ListFooterComponent = ()=>{
+    return(
+    <View>
+      { this.state.isLoading ?
+        <Text>Loading...</Text>
+        :null
+      }
+    </View>
+      // animating={this.state.isLoading == true ? true : false} 
+      // size="large" 
+      // color="#0000ff"/> 
+    )
+  }
+  searchedData = ({item,index}) => {
+    return (
+      <View style={{margin:8,backgroundColor:"white"}}>
+        <Text
+          style={{
+            padding:4,
+            borderBottomColor: 'silver',
+            borderBottomWidth: 0.5,
+            margin:4,
+            fontSize:18
+          }}
+        > 
+          {item.bookName} {item.chapterNumber} : {item.verseNumber} 
+        </Text>
+        <Text style={{margin:8,fontSize:16}}>{item.text}</Text>
+      </View>
+  )
+}
+  render() {
+    console.log("isloadoing"+this.state.isLoading+ "DATA LENGTH" +this.state.searchedResult.length)
+    console.log("tabs data "+JSON.stringify(this.state.tabsData))
+    return (
+      <View style={{backgroundColor:"white"}}>
+        <SearchTab
+         toggleFunction={this.toggleButton}
+         activeTab={this.state.activeTab}
+        />
+        <Text style={{alignSelf:"center"}}>{this.state.tabsData.length} searched result found</Text>
+        <ActivityIndicator
+          animating={this.state.isLoading == true ? true : false} 
+          size="large" 
+          color="#0000ff" /> 
+        <FlatList
+          ref={ref => this.elementIndex = ref}
+          data={this.state.tabsData}
+          renderItem={this.searchedData}
+          ListEmptyComponent={this.ListEmptyComponent}
+          ListFooterComponent={this.ListFooterComponent}
+          />
+      </View>
+      
     )
 }
 }
