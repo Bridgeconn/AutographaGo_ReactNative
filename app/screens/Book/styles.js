dd851e1db91e4a9694283d00cfcb579a7d445a62
Prerna Singh <prerna11082@iiitd.ac.in>
2018-06-27 16:52:22 +0530

    updated styles in book

--- app/screens/Book/styles.js
+++ app/screens/Book/styles.js
@@ -2,7 +2,7 @@ import {StyleSheet,Dimensions} from 'react-native'
 import { Icon } from 'native-base';
 const width = Dimensions.get('window').width;
 
-export const numberSelection =(colorFile, sizeFile) =>{
+export const styles =(colorFile, sizeFile) =>{
     return StyleSheet.create({
     container:{
         flex:1,
