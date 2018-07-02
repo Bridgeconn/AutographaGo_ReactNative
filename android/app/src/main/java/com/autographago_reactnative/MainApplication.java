19a4e0a9ce4ffc352f5d662b8db7b395b208b9fa
Prerna Singh <prerna11082@iiitd.ac.in>
2018-07-02 17:29:19 +0530

    added cloud backup lib

--- android/app/src/main/java/com/autographago_reactnative/MainApplication.java
+++ android/app/src/main/java/com/autographago_reactnative/MainApplication.java
@@ -12,6 +12,7 @@ import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.github.alinz.reactnativewebviewbridge.WebViewBridgePackage;
 import com.rnziparchive.RNZipArchivePackage;
+import org.rncloudfs.RNCloudFsPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -32,7 +33,8 @@ public class MainApplication extends Application implements ReactApplication {
             new RealmReactPackage(),
             new VectorIconsPackage(),
             new WebViewBridgePackage(),
-            new RNZipArchivePackage()
+            new RNZipArchivePackage(),
+            new RNCloudFsPackage()
       );
     }
 
